f7338bd7e7c45b1391ddffebfad719f7ee36f12d
AidanBaird <102356055+AidanBaird@users.noreply.github.com>
2022-03-25 10:06:24 +0000

    Add files via upload

---
+++ 1-100_random_number_generator.py
@@ -0,0 +1,28 @@
+import random
+x = (random.randint(1,100))
+
+print(x)
+
+if x < 10:
+    print("Less than 10")
+elif x < 20:
+    print("11-20")
+elif x < 30:
+    print("21-30")
+elif x < 40:
+    print("31-40")
+elif x < 50:
+    print("41-50")
+elif x < 60:
+    print("51-60")
+elif x < 70:
+    print("61-70")
+elif x < 80:
+    print("71-80")
+elif x < 90:
+    print("81-90")
+else:
+    print("91-100")
+
+
+
